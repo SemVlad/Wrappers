eb230995345a4f0df0c3010cd9b634ba53335afc
SemVlad <s.vov.sem@gmail.com>
2022-05-10 10:46:40 +0300

    all

---
+++ src/com/company/wrappers/StringTest.java
@@ -0,0 +1,98 @@
+package com.company.wrappers;
+
+import java.util.Scanner;
+
+public class StringTest {
+    public static void main(String[] args) {
+
+        // Создаем строки разными способами
+        String st1 = "Привет";
+        String st2 = new String("Байрактар");
+
+        // Массив символов
+        char[] st3 = {'d', 'd', 't'};
+        String st4 = new String(st3);
+
+        // Из байта
+        String st5 = "dfa";
+        String st6 = new String(st5.getBytes());
+
+        // С помощью StringBuilder
+        StringBuilder st7 = new StringBuilder("fdasf");
+        String st8 = st7.toString();
+
+        // Работа со строкой
+        // Ищем самое длинное слово
+        String str1 = "Апельсин,Яблоко,Гранат,Груша";
+        String[] str2 = str1.split(",");      // Делим строку по признаку и создаем масив строк
+        int[] num = new int[str2.length];           // Массив с количеством символов в каждом слове
+        for (int i = 0; i < str2.length; i++) {
+            num[i] = str2[i].length();
+            System.out.println(num[i]);
+        }
+
+        // Ищем самое длинное слово по количеству символов в нем
+        int max = num[3];
+        for (int j = 0; j < num.length; j++) {
+            if (max < num[j]) {
+                max = num[j];
+                System.out.println("Максимальная длинна слова в массиве под индексом = " + j);
+                System.out.println("Этому индексу соответствует слово - " + str2[j].toLowerCase()); // нижний регистр
+
+                // Выводим "обрезанную" строку)
+                System.out.println(str2[j].substring(0, 3));
+            }
+        }
+
+        // Играемся со строкой
+        String newStr = "  Я_новая_строка   ";
+        System.out.println(newStr.trim().replace("_", " "));
+
+        // Работаем со сканером
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Введите текст: ");
+        String text = scan.nextLine();
+        System.out.println("Вы ввели: " + text);
+
+        // В смысл вводимой фразы заложен запуск "чего-то" на мордовию)
+        String[] arrText = text.split(" ");         // Разбиваем на масив для дальнейшей проверки
+        String wordStart = "Запуск";
+        String wordEnd = "завершен";
+        String wordErr = "ошибка";
+        if (arrText[0].equals(wordStart)) {             // Начало проверок
+            System.out.println("Запускаем процесс");
+        } else {
+            System.out.println("може москалям повезло?");
+            if (arrText[arrText.length - 1].equals(wordEnd)) {
+                System.out.println("Процесс завершен... не повезло)");
+            } else {
+                System.out.println("ще на підльоті...");
+            }
+            for (String s : arrText) {
+                if (s.equalsIgnoreCase(wordErr)) {
+                    System.out.println("Произошла ошибка " + "какая досада...");
+                }
+            }
+            System.out.println(".....");
+        }
+
+        // Работа с StringBuilder
+        StringBuilder bd = new StringBuilder();
+        bd.append(st1).append(st2).append(st3).append('\n');
+        bd.append(st4).append(st5).append(st6).append('\n');
+        bd.append(st7).append(st8).append(newStr).append('\n');
+        bd.append(wordEnd).append(wordErr).append(wordStart).append('\n');
+        bd.append(str1);
+
+        bd.reverse();   // Разворачиваем все задом на перед
+
+        // Не получилось найти спец метод, сделал ручками
+        // Делаем все в одну строчку
+        for (int i = 0; i < bd.length(); i++) {
+            if (String.valueOf(bd.charAt(i)).equals("\n")) {
+                bd.deleteCharAt(i);
+            }
+        }
+        System.out.println(bd);
+    }
+}
---
+++ src/com/company/wrappers/WrapperTest.java
@@ -0,0 +1,80 @@
+package com.company.wrappers;
+
+public class WrapperTest {
+    public static void main(String[] args) {
+
+        Byte b1 = 13;
+        Byte b2 = Byte.valueOf((byte) 13);
+        Byte b3 = new Byte((byte) 13);
+        Byte b4 = Byte.parseByte("13");
+
+        Short sh1 = 13;
+        Short sh2 = Short.valueOf((short) 13);
+        Short sh3 = new Short((short) 13);
+        Short sh4 = Short.parseShort("13");
+
+        Integer i1 = 13;
+        Integer i2 = Integer.valueOf(13);
+        Integer i3 = new Integer(13);
+        Integer i4 = Integer.parseInt("13");
+
+        Long l1 = 13L;
+        Long l2 = Long.valueOf(13L);
+        Long l3 = new Long(13L);
+        Long l4 = Long.parseLong("13");
+
+        Float f1 = 13.0f;
+        Float f2 = Float.valueOf(13.0f);
+        Float f3 = new Float(13.0f);
+        Float f4 = Float.parseFloat("13.0f");
+
+        Double d1 = 13.0;
+        Double d2 = Double.valueOf(13.0);
+        Double d3 = new Double(13.0);
+        Double d4 = Double.parseDouble("13.0");
+
+        Boolean isB1 = true;
+        Boolean isB2 = Boolean.valueOf(true);
+        Boolean isB3 = new Boolean(false);
+        Boolean isB4 = Boolean.parseBoolean("false");
+
+        Character ch1 = 'c';
+        Character ch2 = new Character('e');
+        Character ch3 = Character.valueOf('g');
+
+        // С одного типа в разные
+        Double db = 13.13;
+        byte by = db.byteValue();
+        short sh = db.shortValue();
+        int i = db.intValue();
+        long l = db.longValue();
+        float fl = 13.13F;
+
+        // NaN/infinity проверки
+        Double dab1 = 0.0;
+        Double dab2 = 1.0;
+        Double nanValue = dab2 / dab1;
+        Double infinityValue = dab1 / dab1;
+        if (nanValue.isNaN()) {
+            System.out.println("Переменная nanValue = " + "NaN");
+        }
+        if (infinityValue.isNaN()) {
+            System.out.println("Переменная infinityValue = " + "NaN");
+        }
+        if (nanValue.isInfinite()) {
+            System.out.println("Переменная nanValue = " + "Infinity");
+        }
+        if (infinityValue.isInfinite()) {
+            System.out.println("Переменная infinityValue = " + "Infinity");
+        }
+
+        // Еще что-то)
+        Long ln1 = 120L;
+        Long ln2 = 120L;
+        System.out.println(ln1 == ln2);
+
+        Long ln3 = 1200L;
+        Long ln4 = 1200L;
+        System.out.println(ln3 == ln4);
+    }
+}
